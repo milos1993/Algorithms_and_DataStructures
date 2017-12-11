a615d1484546e04046115a38a0678d85b1298da3
MilosDragovic <acer e1@SOMI>
2017-12-11 23:41:20 +0100

    add some changes

--- src/inf_algorithms/ISortingAlgorithms.java
+++ src/inf_algorithms/ISortingAlgorithms.java
@@ -12,5 +12,5 @@ package  inf_algorithms;
 public interface ISortingAlgorithms {
     int[] selectionSort(int[] arr);
     int[] bubbleSort(int[] arr);
-    int[] megeSort(int[]arr);
+    int[] mergeSort(int[]arr);
 }
